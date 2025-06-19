8a7369e4a6435f11eab0d38aaaf80992def72b87
AnOutcast <160414579+AnOutcast@users.noreply.github.com>
2025-06-19 10:58:10 +0530

    frontend completed

--- frontend/src/components/Newsletter.jsx
+++ frontend/src/components/Newsletter.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Newsletter = () => {
   return (
     <div className='flex flex-col items-center justify-center text-center space-y-2 my-32'>
-      <h1 className='md:text-4x1 text-2xl font-semibold'>Never miss a Blog!</h1>
+      <h1 className='md:text-4xl text-2xl font-semibold'>Never miss a Blog!</h1>
       <p className='md:text-lg text-gray-700/70 pb-8'>Subscribe to get the latest blog, new tech and exclusive news.</p>
       <form className='flex items-center justify-between max-w-2xl w-full md:h-13 h-12'>
         <input className='border border-gray-300 rounded-md h-full border-r-0
