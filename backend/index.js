e3cf8d00aa2b9cfd1432143ea55bc0b44a4e9729
AnOutcast <160414579+AnOutcast@users.noreply.github.com>
2025-06-20 20:08:43 +0530

    setting up login api

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,23 @@
 import express from 'express'
 import 'dotenv/config'
 import cors from 'cors'
+import connectDB from './config/db.js';
+import router from './routes/adminRoutes.js';
 
 const app = express();
+
+await connectDB();
 
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send("Backend is working");
 })
+
+app.use('/api/admin', router);
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port: ${PORT}`)
